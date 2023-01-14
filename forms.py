e9aab481533fd37e9ba26be8685532c62df5bda2
Idan Cohen <idan00189@gmail.com>
2023-01-14 18:20:36 +0200

    initial commit

---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Length,Email
+from flask_ckeditor import CKEditorField
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("כותרת הפוסט", validators=[DataRequired()])
+    subtitle = StringField("כותרת משנה", validators=[DataRequired()])
+    img_url = StringField("קישור לתמונה", validators=[DataRequired(), URL()])
+    body = CKEditorField("תוכן הפוסט", validators=[DataRequired()])
+    submit = SubmitField("הוספה")
+
+class RegisterForm(FlaskForm):
+    email = StringField("אמייל", validators=[DataRequired(),Email()])
+    password = PasswordField("סיסמא", validators=[DataRequired(),Length(min=8,max=64)])
+    name = StringField("שם מלא", validators=[DataRequired()])
+    submit = SubmitField("הרשמה")
+
+class LogInForm(FlaskForm):
+    email = StringField("אמייל", validators=[DataRequired(), Email()])
+    password = PasswordField("סיסמא", validators=[DataRequired(), Length(min=8, max=64)])
+    submit = SubmitField("התחברות")
+
+class CommentForm(FlaskForm):
+    body = CKEditorField("תגובה", validators=[DataRequired()])
+    submit = SubmitField("שלח")
