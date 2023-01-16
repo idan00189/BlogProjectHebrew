8ff89e4185b0dff43ba00a73da607c2fa0f438c3
Idan Cohen <idan00189@gmail.com>
2023-01-16 16:08:29 +0200

    Revert "add reCAPTCHA to the site"
    This reverts commit 9619137a85d76e820c79a3076ac4fd12b137673b.

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL, Length,Email
 from flask_ckeditor import CKEditorField
-from flask_wtf import Form, RecaptchaField
+
 
 ##WTForm
 class CreatePostForm(FlaskForm):
@@ -10,14 +10,12 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("כותרת משנה", validators=[DataRequired()])
     img_url = StringField("קישור לתמונה", validators=[DataRequired(), URL()])
     body = CKEditorField("תוכן הפוסט", validators=[DataRequired()])
-    recaptcha = RecaptchaField()
     submit = SubmitField("הוספה")
 
 class RegisterForm(FlaskForm):
     email = StringField("אמייל", validators=[DataRequired(),Email()])
     password = PasswordField("סיסמא", validators=[DataRequired(),Length(min=8,max=64)])
     name = StringField("שם מלא", validators=[DataRequired()])
-    recaptcha = RecaptchaField()
     submit = SubmitField("הרשמה")
 
 class LogInForm(FlaskForm):
