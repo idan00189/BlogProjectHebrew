9619137a85d76e820c79a3076ac4fd12b137673b
Idan Cohen <idan00189@gmail.com>
2023-01-16 15:16:08 +0200

    add reCAPTCHA to the site

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ login_manager.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['RECAPTCHA_USE_SSL']= True
+app.config['RECAPTCHA_PUBLIC_KEY']= '6LcxQAAkAAAAAGCCCS0Z1I848UdYXrusMlh087v_'
+app.config['RECAPTCHA_PRIVATE_KEY']='cxQAAkAAAAAH2YM4_VBmYIIbYnxd7UQqUBfafP'
+app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
 
 
 
